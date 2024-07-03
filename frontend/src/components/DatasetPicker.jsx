5db7914c4365f573a23f4cbd2a447df64150d459
CodeLionX <sebastian.schmidl@t-online.de>
2024-07-03 19:30:06 +0200

    feat: use dataset API in frontend (+ dev API proxy)

--- frontend/src/components/DatasetPicker.jsx
+++ frontend/src/components/DatasetPicker.jsx
@@ -1,35 +1,19 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {SearchSelect, SearchSelectItem} from "@tremor/react";
 
-function DatasetPicker() {
-  const data = [
-    {
-      id: "ts1",
-      label: "AllGestureWiimoteY",
-      path: "/datasets/ts1.ts",
-    },
-    {
-      id: "ts2",
-      label: "AllGestureWiimoteX",
-      path: "/datasets/ts2.ts",
-    },
-    {
-      id: "ts3",
-      label: "AllGestureWiimoteZ",
-      path: "/datasets/ts3.ts",
-    },
-    {
-      id: "ts4",
-      label: "BeetleFly",
-      path: "/datasets/ts4.ts",
-    },
-  ]
+function DatasetPicker({ onSelect }) {
+  const [datasets, setDatasets] = useState([]);
+  useEffect(() => {
+    fetch("/api/datasets", {method: "GET", headers: {'Content-Type': 'application/json'}, mode: 'no-cors'})
+      .then((res) => res.json())
+      .then((data) => setDatasets(data.datasets));
+  }, []);
 
   return (
     <SearchSelect placeholder="Select dataset" enableClear={true}>
-      {data.map((item) => (
-        <SearchSelectItem key={item.id} value={item.id}>
-          {item.label}
+      {datasets.map((item) => (
+        <SearchSelectItem key={item.id} value={item.id} onClick={e => {e.stopPropagation(); onSelect(item)}}>
+          {item.name}
         </SearchSelectItem>
       ))}
     </SearchSelect>
