5ad0fd806a86ccd5aba3678557660bbe16e9288f
CodeLionX <sebastian.schmidl@t-online.de>
2024-07-25 15:19:14 +0200

    feat: start demo job from frontend

--- frontend/src/components/DatasetPicker.jsx
+++ frontend/src/components/DatasetPicker.jsx
@@ -6,16 +6,24 @@ function DatasetPicker({ onSelect }) {
   useEffect(() => {
     fetch("/api/datasets", {method: "GET", headers: {'Content-Type': 'application/json'}, mode: 'no-cors'})
       .then((res) => res.json())
+      .catch((error) => {
+        console.error("Error fetching datasets:", error);
+        return [];
+      })
       .then((data) => setDatasets(data.datasets));
   }, []);
 
   return (
     <SearchSelect placeholder="Select dataset" enableClear={true}>
-      {datasets.map((item) => (
+      {datasets.length > 0 ? datasets.map((item) => (
         <SearchSelectItem key={item.id} value={item.id} onClick={e => {e.stopPropagation(); onSelect(item)}}>
           {item.name}
         </SearchSelectItem>
-      ))}
+      )) : (
+        <SearchSelectItem key={undefined} value={undefined} aria-disabled={true} disabled={true} className="text-gray-500">
+          No datasets available.
+        </SearchSelectItem>
+      )}
     </SearchSelect>
   );
 }
--- frontend/src/pages/ClusteringPage.jsx
+++ frontend/src/pages/ClusteringPage.jsx
@@ -1,10 +1,12 @@
 import D3BarChart from "../components/D3BarChart";
-import {Button, ProgressBar} from "@tremor/react";
+import {Button} from "@tremor/react";
 import React, {useState} from "react";
 import DatasetPicker from "../components/DatasetPicker";
+import StatusOverview from "../components/StatusOverview";
 
 function ClusteringPage() {
   const [data, setData] = useState([12, 5, 6, 6, 9, 10]);
+  const [dataset, setDataset] = useState(undefined);
 
   const addData = () => {
     setData(d => d.concat([Math.trunc(Math.random()*50)]));
@@ -21,12 +23,24 @@ function ClusteringPage() {
   };
   const progress = data.length * 10;
 
+  const startDemoJob = () => {
+    console.log("Fetching dataset", dataset);
+    fetch("/api/jobs", {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(dataset)
+    })
+      .then(console.log)
+      .catch(console.error);
+  }
+
   return (
-    <div>
+    <div className="m-5">
       <h1 className="text-3xl font-bold">Clustering</h1>
       <div className="flex flex-auto mx-auto">
-        <DatasetPicker/>
+        <DatasetPicker onSelect={dataset => setDataset(dataset)}/>
       </div>
+      <StatusOverview jobId={0} progressPlaceholder={progress}/>
       <D3BarChart data={data}/>
       <div className="flex flex-auto m-auto mx-auto justify-between items-center">
         <Button className="mx-auto" variant="primary" size="md" onClick={addData}>
@@ -39,10 +53,12 @@ function ClusteringPage() {
           +10
         </Button>
       </div>
-      <br/>
-      <div className="flex flex-auto mx-auto">
-        Progress: <ProgressBar value={progress} label={progress.toString() + "%"} className="ml-2" />
+      <div className="flex justify-center items-center m-10">
+        <Button className="mx-auto" variant="primary" size="lg" onClick={startDemoJob}>
+          Start demo Job
+        </Button>
       </div>
+      <br/>
     </div>
   )
 }
