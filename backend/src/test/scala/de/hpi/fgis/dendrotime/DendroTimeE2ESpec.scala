bb86451450663b2be3df3d47358f7689bb6884fd
CodeLionX <sebastian.schmidl@t-online.de>
2024-11-05 18:13:38 +0100

    feat: improve E2E test

--- backend/src/test/scala/de/hpi/fgis/dendrotime/DendroTimeE2ESpec.scala
+++ backend/src/test/scala/de/hpi/fgis/dendrotime/DendroTimeE2ESpec.scala
@@ -11,6 +11,7 @@ import org.scalatest.wordspec.AnyWordSpecLike
 import java.io.FileNotFoundException
 import scala.concurrent.duration.*
 import scala.language.postfixOps
+import scala.util.{Failure, Success, Try}
 
 class DendroTimeE2ESpec extends ScalaTestWithActorTestKit with AnyWordSpecLike with should.Matchers with LogCapturing {
 
@@ -24,43 +25,44 @@ class DendroTimeE2ESpec extends ScalaTestWithActorTestKit with AnyWordSpecLike w
 
   private def performSystemTest(dataset: Dataset, params: DendroTimeParams, gtFile: String): Unit = {
     // load ground truth
-    try {
-      val gtHierarchy = TestUtil.loadHierarchy(gtFile)
+    Try {
+       TestUtil.loadHierarchy(gtFile)
+    } match {
+      case Success(gtHierarchy) =>
+        // start DendroTime
+        val guardianProbe = createTestProbe[Scheduler.Response]()
+        val dendroTimeScheduler = spawn(Scheduler(), s"scheduler-${dataset.id}")
+        dendroTimeScheduler ! Scheduler.StartProcessing(dataset, params, guardianProbe.ref)
+        guardianProbe.expectMessageType[Scheduler.ProcessingStarted](100 millis)
 
-      // start DendroTime
-      val guardianProbe = createTestProbe[Scheduler.Response]()
-      val dendroTimeScheduler = spawn(Scheduler(), s"scheduler-${dataset.id}")
-      dendroTimeScheduler ! Scheduler.StartProcessing(dataset, params, guardianProbe.ref)
-      guardianProbe.expectMessageType[Scheduler.ProcessingStarted](100 millis)
+        // wait for processing to finish and return final hierarchy
+        val progressProbe = createTestProbe[ProgressMessage]()
+        val timer = testKit.scheduler.scheduleWithFixedDelay(0 seconds, 200 millis) { () =>
+          dendroTimeScheduler ! Scheduler.GetProgress(dataset.id, progressProbe.ref)
+        }
+        val messages = progressProbe.fishForMessage(60 seconds) {
+          case ProgressMessage.CurrentProgress(Status.Finished, 100, _, _, _, _, _, _) =>
+            timer.cancel()
+            FishingOutcomes.complete
+          case _ =>
+            FishingOutcomes.continueAndIgnore
+        }
+        messages.length should be > 0
+        val finalMessage = messages.last
 
-      // wait for processing to finish and return final hierarchy
-      val progressProbe = createTestProbe[ProgressMessage]()
-      val timer = testKit.scheduler.scheduleWithFixedDelay(0 seconds, 200 millis) { () =>
-        dendroTimeScheduler ! Scheduler.GetProgress(dataset.id, progressProbe.ref)
-      }
-      val messages = progressProbe.fishForMessage(60 seconds) {
-        case ProgressMessage.CurrentProgress(Status.Finished, 100, _, _, _, _, _, _) =>
-          timer.cancel()
-          FishingOutcomes.complete
-        case _ =>
-          FishingOutcomes.continueAndIgnore
-      }
-      messages.length should be > 0
-      val finalMessage = messages.last
+        // compare hierarchy with ground truth
+        finalMessage shouldBe a[ProgressMessage.CurrentProgress]
+        val progress = finalMessage.asInstanceOf[ProgressMessage.CurrentProgress]
+        progress.state shouldBe Status.Finished
+        progress.progress shouldBe 100
+        progress.hierarchy shouldEqual gtHierarchy
 
-      // compare hierarchy with ground truth
-      finalMessage shouldBe a[ProgressMessage.CurrentProgress]
-      val progress = finalMessage.asInstanceOf[ProgressMessage.CurrentProgress]
-      progress.state shouldBe Status.Finished
-      progress.progress shouldBe 100
-      progress.hierarchy shouldEqual gtHierarchy
-
-      testKit.stop(dendroTimeScheduler)
-      guardianProbe.expectTerminated(dendroTimeScheduler, 100 millis)
-
-    } catch {
-      case _: FileNotFoundException =>
+        testKit.stop(dendroTimeScheduler)
+        guardianProbe.expectTerminated(dendroTimeScheduler, 100 millis)
+      case Failure(_: FileNotFoundException) =>
         cancel(s"Ground truth file not found: $gtFile")
+      case Failure(e) =>
+        fail(e)
     }
   }
 
