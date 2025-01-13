a4da4ce5403bb861c99379c9558da6c94678d311
CodeLionX <sebastian.schmidl@t-online.de>
2025-01-13 13:00:22 +0100

    fix: parallelize serial experiment

--- experiments/01-serial-hac/run.sh
+++ experiments/01-serial-hac/run.sh
@@ -6,8 +6,11 @@ distances=( "euclidean" "dtw" "msm" "sbd" )
 linkages=( "single" "complete" "average" "ward" )
 # download datasets
 datasets=$(python download-all-datasets.py)
+# set parallelization factor to number of physical cores
+N=$(lscpu -p | grep -v '^#' | cut -d, -f2 | sort -n | uniq | wc -l)
+echo "Number of physical cores: $N"
 
-# run experiments
+# run experiments in N subprocesses
 for dataset in $datasets; do
   for distance in "${distances[@]}"; do
     for linkage in "${linkages[@]}"; do
@@ -17,11 +20,18 @@ for dataset in $datasets; do
       java -Xmx16g -Dfile.encoding=UTF-8 \
         -Dlogback.configurationFile=../logback.xml \
         -Dconfig.file=application.conf \
-        -jar ../DendroTime-runner.jar --serial --dataset "${dataset}" --distance "${distance}" --linkage "${linkage}"
-      exit 0
+        -jar ../DendroTime-runner.jar --serial --dataset "${dataset}" --distance "${distance}" --linkage "${linkage}" &
+
+      # allow to execute up to $N jobs in parallel
+      if [[ $(jobs -r -p | wc -l) -ge $N ]]; then
+          wait -n
+      fi
     done
   done
 done
+
+# wait for the remaining jobs to finish
+wait
 
 # aggregate runtime results
 python aggregate-runtimes.py
--- experiments/run.sh
+++ experiments/run.sh
@@ -2,7 +2,7 @@
 
 java -Xmx48g -Dfile.encoding=UTF-8 \
     -Dlogback.configurationFile=logback.xml \
-    -Dconfig.file=application.conf \
+    -Dconfig.file=common.conf \
     -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 \
     -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false \
     -jar DendroTime-runner.jar $@
