13d3e7d59ab285b81068b45a1927071f2bb6d949
CodeLionX <sebastian.schmidl@t-online.de>
2025-02-13 16:55:29 +0100

    feat: parallelize runtime aggregation script

--- experiments/01-serial-hac/aggregate-runtimes.py
+++ experiments/01-serial-hac/aggregate-runtimes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import joblib
 
 from pathlib import Path
 from tqdm import tqdm
@@ -12,8 +13,12 @@ from scipy.cluster.hierarchy import cut_tree
 from sklearn.metrics import adjusted_rand_score
 from aeon.datasets import load_classification
 
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from tqdm_joblib import tqdm_joblib
+
 DATA_FOLDER = Path("../data/datasets")
 RESULT_FOLDER = Path("results")
+N_JOBS = 4
 
 
 @dataclass
@@ -24,21 +29,37 @@ class Experiment:
     strategy: str = "serial"
 
 
-def parse_experiment_name(f):
+def _parse_experiment_name(f):
     parts = f.stem.split("-")
     return Experiment(*parts)
 
 
-def evaluate_clustering(exp, file):
-    try:
-        _, y = load_classification(exp.dataset, extract_path=DATA_FOLDER, load_equal_length=False)
-        n_clusters = len(np.unique(y))
-        Z = np.loadtxt(file / "serial" / "hierarchy.csv", delimiter=",")
-        clusters = cut_tree(Z, n_clusters=n_clusters).flatten()
-        return adjusted_rand_score(y, clusters)
-    except (FileNotFoundError, ValueError) as e:
-        print(f"Failed to compute quality for {exp}: {e}")
-        return np.nan
+def _evaluate_clustering(exp, file):
+    ari = np.nan
+    if not exp.dataset.startswith("edeniss"):
+        try:
+            _, y = load_classification(
+                exp.dataset, extract_path=DATA_FOLDER, load_equal_length=False
+            )
+            n_clusters = len(np.unique(y))
+            Z = np.loadtxt(file / "serial" / "hierarchy.csv", delimiter=",")
+            clusters = cut_tree(Z, n_clusters=n_clusters).flatten()
+            ari = adjusted_rand_score(y, clusters)
+        except (FileNotFoundError, ValueError) as e:
+            print(f"Failed to compute quality for {exp}: {e}")
+    return ari
+
+
+def extract_results(file):
+    exp = _parse_experiment_name(file)
+    exp_runtimes = pd.read_csv(file / "serial" / "runtimes.csv")
+    ari = _evaluate_clustering(exp, file)
+    entries = []
+    for phase, runtime in exp_runtimes.itertuples(index=False):
+        entries.append(
+            (exp.dataset, exp.distance, exp.linkage, exp.strategy, phase, runtime, ari)
+        )
+    return entries
 
 
 def main():
@@ -47,17 +68,22 @@ def main():
         f"Processing results from {len(experiments)} experiments ...", file=sys.stderr
     )
     entries = []
-    for file in tqdm(experiments):
-        exp = parse_experiment_name(file)
-        exp_runtimes = pd.read_csv(file / "serial" / "runtimes.csv")
-        ari = evaluate_clustering(exp, file)
-        for phase, runtime in exp_runtimes.itertuples(index=False):
-            entries.append(
-                (exp.dataset, exp.distance, exp.linkage, exp.strategy, phase, runtime, ari)
-            )
+    with tqdm_joblib(tqdm(desc="Processing", total=len(experiments))):
+        entries = joblib.Parallel(n_jobs=N_JOBS)(
+            joblib.delayed(extract_results)(f) for f in experiments
+        )
+
     df = pd.DataFrame(
-        entries,
-        columns=["dataset", "distance", "linkage", "strategy", "phase", "runtime", "ARI"],
+        [e for batch in entries for e in batch],
+        columns=[
+            "dataset",
+            "distance",
+            "linkage",
+            "strategy",
+            "phase",
+            "runtime",
+            "ARI",
+        ],
     )
     df.to_csv(RESULT_FOLDER / "aggregated-runtimes.csv", index=False)
     print("... done.", file=sys.stderr)
